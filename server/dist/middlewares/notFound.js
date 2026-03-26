15cfa617c2cc2df563e54b3a0c212be3b6cc3652
jaip006 <yadavjp061202@gmail.com>
2026-03-26 22:39:00 +0530

    chore: ignore and untrack env/build artifacts

--- server/dist/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundMiddleware = (req, res, next) => {
-    console.log("---notFoundMiddleware---");
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-};
-exports.default = notFoundMiddleware;
